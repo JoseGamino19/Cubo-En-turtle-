3fcf63996cbb288357e11f1b6556523b4bc173bb
JoseGamino19 <115297933+JoseGamino19@users.noreply.github.com>
2025-02-13 10:33:40 -0800

    Add files via upload
    archivo con una funcion donde se crea un cubo 3D

---
+++ cubo.py
@@ -0,0 +1,37 @@
+from turtle import *
+
+setup(800, 600, 0, 0)
+screensize(800, 600)
+
+
+def constriurCubo():
+    goto(0, 100)
+    goto(100,100)
+    goto(100,0)
+    goto(0,0)
+
+    goto(0,0)
+    goto(40,40)
+
+    goto(40,140)
+    goto(140,140) 
+    goto(140,40) 
+    goto(40,40) 
+    goto(140,40) 
+    goto(100,0) 
+    goto(100,100) 
+    goto(100,100) 
+    goto(140,140) 
+    goto(40,140)
+    goto(0,100)
+    
+constriurCubo()
+
+
+
+
+
+
+
+
+exitonclick()
